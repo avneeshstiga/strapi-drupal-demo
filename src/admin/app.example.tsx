7dd232ffe9fec5c58b54a5892d4fba4392273493
MohanStiga <mohan.pruthi@stigasoft.com>
2025-05-19 15:19:35 +0530

    Added SEO plugin

--- src/admin/app.example.tsx
+++ src/admin/app.example.tsx
@@ -2,34 +2,21 @@ import type { StrapiApp } from '@strapi/strapi/admin';
 
 export default {
   config: {
-    locales: [
-      // 'ar',
-      // 'fr',
-      // 'cs',
-      // 'de',
-      // 'dk',
-      // 'es',
-      // 'he',
-      // 'id',
-      // 'it',
-      // 'ja',
-      // 'ko',
-      // 'ms',
-      // 'nl',
-      // 'no',
-      // 'pl',
-      // 'pt-BR',
-      // 'pt',
-      // 'ru',
-      // 'sk',
-      // 'sv',
-      // 'th',
-      // 'tr',
-      // 'uk',
-      // 'vi',
-      // 'zh-Hans',
-      // 'zh',
-    ],
+    locales: ['en', 'de'], // Enable English and German
+    translations: {
+      en: {
+        'Auth.form.email.label': 'Email address',
+        Users: 'Users',
+        City: 'City',
+        Id: 'ID',
+      },
+      de: {
+        'Auth.form.email.label': 'E-Mail-Adresse',
+        Users: 'Benutzer',
+        City: 'Stadt',
+        Id: 'ID',
+      },
+    },
   },
   bootstrap(app: StrapiApp) {
     console.log(app);
